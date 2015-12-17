6fc868834652068ed50c20010157742be7c36575
Br. Samuel Springuel <rpspringuel@gmail.com>
2015-12-17 15:50:09 -0500

    Differentiating between normal and backwards tests when accepting
    We assume that all backwards tests have `_B` in the filename so that we can copy their expectations into backwards instead of tests
    Using this mechanism we accept the changes in the proof-of-concept test

--- harness.sh
+++ harness.sh
@@ -103,7 +103,14 @@ function maybe_run {
 
 function accept_result {
     echo "Accepting $2 as expectation for $1"
-    $CP "$2" "$testroot/tests/$(dirname "$1")/$3"
+    if [[ "$1" = *"_B"* ]]
+    then
+        # this is a backwards test so it needs to be copied to backwards not tests
+        accept_dest="backwards"
+    else
+        accept_dest="tests"
+    fi
+    $CP "$2" "$testroot/$accept_dest/$(dirname "$1")/$3"
 }
 
 function view_text {
