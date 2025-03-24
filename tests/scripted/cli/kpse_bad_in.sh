ab4d6fff9cfae436683539a8752868e7b854b8eb
Fr. Samuel Springuel <rpspringuel@gmail.com>
2025-03-24 19:38:42 -0400

    Add error messages to tests.

--- tests/scripted/cli/kpse_bad_in.sh
+++ tests/scripted/cli/kpse_bad_in.sh
@@ -6,5 +6,7 @@ then
     else exit 0
     fi
 else
+    >&2 echo "$gregorio does not use the kpathsea libraries"
+    >&2 echo "unable to run test kpse_bad_in.sh"
     exit 2
 fi
--- tests/scripted/cli/kpse_ok.sh
+++ tests/scripted/cli/kpse_ok.sh
@@ -4,5 +4,7 @@ then
     eval $gregorio -S test.gabc
     exit $?
 else
+    >&2 echo "$gregorio does not use the kpathsea libraries"
+    >&2 echo "unable to run test kpse_ok.sh"
     exit 2
 fi
