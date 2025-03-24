5245f252ed6598c7a374d34aa791af317d9cf6df
Fr. Samuel Springuel <rpspringuel@gmail.com>
2025-03-24 14:55:06 -0400

    Correct output

--- tests/scripted/cli/stdin_and_in.sh
+++ tests/scripted/cli/stdin_and_in.sh
@@ -1,6 +1,7 @@
-EXPECTED="$gregorio: missing file operand.
+EXPECTED="warning: can't read from both stdin and a file, reading from test.gabc
 Usage: $gregorio [OPTION]... [-s | INPUT_FILE]
-Try '$gregorio --help' for more information."
+Try '$gregorio --help' for more information.
+Proceeding anyway..."
 
 OUTCOME=$(eval $gregorio -s test.gabc 2>&1)
 
