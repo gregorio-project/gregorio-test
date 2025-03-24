a145411979a388b124faf7bec335dd6a2d39c316
Fr. Samuel Springuel <rpspringuel@gmail.com>
2025-03-24 15:31:10 -0400

    Switch to bash regex tests to make tests version agnostic
    (cherry picked from commit 3ede73b4012a651c385e13f8911fb3e190be5935)

--- tests/scripted/cli/version.sh
+++ tests/scripted/cli/version.sh
@@ -1,11 +1,9 @@
-EXPECTEDOUT="Gregorio 6.0.0.
-Copyright (C) 2006-2021 Gregorio Project authors (see CONTRIBUTORS.md)
-License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+EXPECTEDOUT="Gregorio [0-9]\.[0-9]\.[0-9](-[a-zA-Z0-9.-]{1,}-[0-9a-f]{8}-[0-9]{1,})?( \(kpathsea version [0-9]\.[0-9]\.[0-9]\))?\.
+Copyright \(C\) 2006-202[0-9] Gregorio Project authors \(see CONTRIBUTORS\.md\)
+License GPLv3\+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl\.html>
 This is free software: you are free to change and redistribute it.
-There is NO WARRANTY, to the extent permitted by law."
+There is NO WARRANTY, to the extent permitted by law\."
 
 OUTCOMEOUT=$(eval $gregorio -V)
-OUTCOMEOUT=$(sed '1 s/ (.*)//g' <<<$OUTCOMEOUT) #remove kpathsea version info
-OUTCOMEOUT=$(sed '1 s/-[^.]*//' <<<$OUTCOMEOUT) #remove git commit info
 
-[[ "$EXPECTEDOUT" == "$OUTCOMEOUT" ]] || exit 1
+[[ "$OUTCOMEOUT" =~ $EXPECTEDOUT ]] || exit 1
