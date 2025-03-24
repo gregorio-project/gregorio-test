6a0ecd5beaaf5c4234a20ba4b9a7813adc8b019c
Fr. Samuel Springuel <rpspringuel@gmail.com>
2025-03-24 17:10:16 -0400

    More rewritten tests

--- tests/scripted/cli/multi_out.sh
+++ tests/scripted/cli/multi_out.sh
@@ -1,2 +1,8 @@
-eval $gregorio -o out1.out -o out2.out test.gabc
-exit $?
+EXPECTED="warning: several output files declared, out1.out taken
+Usage: $gregorio [OPTION]... [-s | INPUT_FILE]
+Try '$gregorio --help' for more information.
+Proceeding anyway..."
+
+OUTCOME=$(eval $gregorio -o out1.out -o out2.out test.gabc 2>&1 1> /dev/null)
+
+[[ "$EXPECTED" == "$OUTCOME" ]] || exit 1
