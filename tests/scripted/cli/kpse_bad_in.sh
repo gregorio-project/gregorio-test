ba528992c289135beeac223ead89355286e9f612
Fr. Samuel Springuel <rpspringuel@gmail.com>
2025-03-24 20:23:17 -0400

    kpse tests rewritten

--- tests/scripted/cli/kpse_bad_in.sh
+++ tests/scripted/cli/kpse_bad_in.sh
@@ -1,10 +1,11 @@
 if eval $gregorio -V | grep -q kpathsea
 then
     export TEXMFCNF="$PWD:"
-    if eval $gregorio -S "$PWD/test.gabc"
-    then exit 1
-    else exit 0
-    fi
+    EXPECTED="error:kpse prohibits read from file $PWD/test.gabc"
+    
+    OUTCOME=$(eval $gregorio -S "$PWD/test.gabc" 2>&1 1> /dev/null)
+    
+    [[ "$OUTCOME" =~ $EXPECTED ]] || exit 1
 else
     exit 0
 fi
--- tests/scripted/cli/kpse_ok.sh
+++ tests/scripted/cli/kpse_ok.sh
@@ -1,8 +1,11 @@
 if eval $gregorio -V | grep -q kpathsea
 then
     export TEXMFCNF="$PWD:"
-    eval $gregorio -S test.gabc
-    exit $?
+    source test-gtex.rc
+    
+    OUTCOME=$($gregorio -S test.gabc)
+    
+    [[ "$OUTCOME" =~ $EXPECTED ]] || exit 1
 else
     exit 0
 fi
