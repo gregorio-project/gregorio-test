24950222ddec11ed2cd07f0155fded635128e5b3
David Chiang <dchiang@nd.edu>
2026-02-03 23:10:58 -0500

    Add test for https://github.com/gregorio-project/gregorio/pull/1692

---
+++ tests/scripted/recorder/recorder.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+lualatex --shell-escape --interaction=nonstopmode --recorder PopulusSion
+EXPECTED="INPUT PopulusSion.gabc
+OUTPUT PopulusSion.gaux"
+OUTCOME=$(egrep 'PopulusSion\.(gabc|gaux)' PopulusSion.fls | sort -u)
+
+echo "EXPECTED: $EXPECTED"
+echo "OUTCOME: $OUTCOME"
+
+[[ "$OUTCOME" == "$EXPECTED" ]] || exit 1
+
+lualatex --shell-escape --interaction=nonstopmode --recorder PopulusSion
+EXPECTED="INPUT PopulusSion.gaux"
+OUTCOME=$(egrep 'PopulusSion\.(gabc|gaux)' PopulusSion.fls | sort -u)
+
+echo "EXPECTED: $EXPECTED"
+echo "OUTCOME: $OUTCOME"
+
+[[ "$OUTCOME" == "$EXPECTED" ]] || exit 1
+
