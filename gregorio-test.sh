42d8772b8c6f274a1026f66032bff67526c5baf9
Fr. Samuel Springuel <rpspringuel@gmail.com>
2026-03-27 20:08:32 -0400

    Ensure the pgid isn't interpreted as a flag

--- gregorio-test.sh
+++ gregorio-test.sh
@@ -511,7 +511,7 @@ test|retest)
     bg_pgid=$(ps -o pgid= -p $bg_pid | tr -d ' ')    
     
     # Set up trap to kill background jobs on interrupt
-    trap "kill -TERM -$bg_pgid 2>/dev/null; echo; exit 130" INT
+    trap "kill -TERM -- -$bg_pgid 2>/dev/null; echo; exit 130" INT
     
     if $progress_bar
     then
