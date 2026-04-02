d40d158949b58dd42e9e6d1b571201f5c08aabf1
Fr. Samuel Springuel <rpspringuel@gmail.com>
2026-04-02 11:48:19 -0400

    Change position of beta flag in regex

--- tests/scripted/cli/version.sh
+++ tests/scripted/cli/version.sh
@@ -1,4 +1,4 @@
-EXPECTED="Gregorio [0-9]\.[0-9]\.[0-9](-[a-zA-Z0-9./-]{1,}-[0-9a-f]{8}-[0-9]{1,})?(-beta[0-9]{1,})?( \(kpathsea version [0-9]\.[0-9]\.[0-9]\))?\.
+EXPECTED="Gregorio [0-9]\.[0-9]\.[0-9](-beta[0-9]{1,})?(-[a-zA-Z0-9./-_]{1,}-[0-9a-f]{8}-[0-9]{1,})?( \(kpathsea version [0-9]\.[0-9]\.[0-9]\))?\.
 Copyright \(C\) 2006-202[0-9] Gregorio Project authors \(see CONTRIBUTORS\.md\)
 License GPLv3\+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl\.html>
 This is free software: you are free to change and redistribute it.
