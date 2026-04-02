6a7f1437c74e9385b5ea389c28825c8c69f7b71d
Fr. Samuel Springuel <rpspringuel@gmail.com>
2026-04-02 11:47:38 -0400

    Debugging output on failing tests

--- tests/scripted/cli/invalid_option.sh
+++ tests/scripted/cli/invalid_option.sh
@@ -3,7 +3,10 @@ $gregorio: invalid option -- Z
 error: $gregorio: missing file operand.
 Usage: $gregorio [OPTION]... [-s | INPUT_FILE]
 Try '$gregorio --help' for more information."
+echo $EXPECTED
 
 OUTCOME=$(eval $gregorio -Z 2>&1 1> /dev/null)
+echo
+echo $OUTCOME
 
 [[ "$EXPECTED" == "$OUTCOME" ]] || exit 1
--- tests/scripted/cli/version.sh
+++ tests/scripted/cli/version.sh
@@ -3,7 +3,10 @@ Copyright \(C\) 2006-202[0-9] Gregorio Project authors \(see CONTRIBUTORS\.md\)
 License GPLv3\+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl\.html>
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law\."
+echo $EXPECTED
 
 OUTCOME=$(eval $gregorio -V)
+echo
+echo $OUTCOME
 
 [[ "$OUTCOME" =~ $EXPECTED ]] || exit 1
