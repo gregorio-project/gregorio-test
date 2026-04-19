f98ee31565fff5255bdf261efc4da0ffc1684ec2
Fr. Samuel Springuel <rpspringuel@gmail.com>
2026-04-19 17:01:28 -0400

    Escape periods
    This output had a couple of unescaped periods in it.

--- tests/scripted/cli/version.sh
+++ tests/scripted/cli/version.sh
@@ -1,7 +1,7 @@
 EXPECTED="Gregorio [0-9]\.[0-9]\.[0-9](-(beta|rc)[0-9]{1,})?(-[a-zA-Z0-9./_-]{1,}-[0-9a-f]{8}-[0-9]{1,})?( \(kpathsea version [0-9]\.[0-9]\.[0-9]\))?\.
 Copyright \(C\) 2006-202[0-9] Gregorio Project authors \(see CONTRIBUTORS\.md\)
-License GPLv3\+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl\.html>
-This is free software: you are free to change and redistribute it.
+License GPLv3\+: GNU GPL version 3 or later <http://gnu\.org/licenses/gpl\.html>
+This is free software: you are free to change and redistribute it\.
 There is NO WARRANTY, to the extent permitted by law\."
 echo "$EXPECTED"
 
